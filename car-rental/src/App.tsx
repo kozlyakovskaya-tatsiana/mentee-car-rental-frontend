bbf4285ca7c5fc88ac72f238bdfc638b9604725f
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-01-13 10:15:39 +0300

    theme styles moved on App component level

--- car-rental/src/App.tsx
+++ car-rental/src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
 
 import Header from './components/Header'
 import Main from './components/pages/Main'
 import Login from './components/pages/Login'
+import Register from './components/pages/Register'
 
 import './App.scss'
+import themeOptions from './styles'
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/Login" element={<Login />} />
-            </Routes>
-        </div>
+        <ThemeProvider theme={themeOptions}>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Register" element={<Register />} />
+                </Routes>
+            </div>
+        </ThemeProvider>
     )
 }
 
--- car-rental/src/components/pages/Login/index.tsx
+++ car-rental/src/components/pages/Login/index.tsx
@@ -2,21 +2,18 @@ import React from 'react'
 
 import Container from '@mui/material/Container'
 import { ThemeProvider } from '@mui/material/styles'
-import { useForm, SubmitHandler } from 'react-hook-form'
 
-import themeOptions from './styles'
+import themeOptions from 'styles'
 
 import LoginForm from './LoginForm'
 import Copyright from './copyright'
 
 function Login() {
     return (
-        <ThemeProvider theme={themeOptions}>
-            <Container maxWidth="xs">
-                <LoginForm />
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
+        <Container maxWidth="xs">
+            <LoginForm />
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
     )
 }
 

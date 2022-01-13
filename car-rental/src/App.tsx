7fba7654a6a15ee60650ce0632f98e2fec8572bf
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-01-13 17:17:03 +0300

    Created global theme options context

--- car-rental/src/App.tsx
+++ car-rental/src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { ThemeProvider } from '@mui/material'
+
 import Header from './components/Header'
 import Main from './pages/Main'
 import Authorize from './pages/Authorize'
 
 import './App.scss'
+import themeOptions from './Theme'
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/Login" element={<Authorize />} />
-            </Routes>
+            <ThemeProvider theme={themeOptions}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/Login" element={<Authorize />} />
+                </Routes>
+            </ThemeProvider>
         </div>
     )
 }

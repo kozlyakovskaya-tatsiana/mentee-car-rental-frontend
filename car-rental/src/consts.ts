a06a99a1af5a84eb77460e5a21afafbcb194906b
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-02-02 11:15:26 +0300

    Added delete request for Rental Points

--- car-rental/src/consts.ts
+++ car-rental/src/consts.ts
@@ -11,3 +11,4 @@ export const GET_ALL_CITIES_REQUEST_URL = `${API_URL}/location/city`
 
 export const GET_ALL_RENTAL_POINTS_REQUEST_URL = `${API_URL}/rentalpoint`
 export const CREATE_RENTAL_POINT_POST_REQUEST_URL = `${API_URL}/rentalpoint`
+export const DELETE_RENTAL_POINT_REQUEST_URL = `${API_URL}/rentalpoint`
--- car-rental/src/services/rentalPoint.service.ts
+++ car-rental/src/services/rentalPoint.service.ts
@@ -1,32 +1,50 @@
 import axios from 'axios'
 import {
     CREATE_RENTAL_POINT_POST_REQUEST_URL,
+    DELETE_RENTAL_POINT_REQUEST_URL,
     GET_ALL_RENTAL_POINTS_REQUEST_URL,
 } from 'consts'
 import { Address } from 'shared/types/RentalPoint'
-import { Coordinate } from '../shared/types/Locations'
 
 export const getRentalPoints = () => {
     return axios.get(`${GET_ALL_RENTAL_POINTS_REQUEST_URL}`, {})
 }
 
+interface CreateRentalPointLocation {
+    country: string
+    city: string
+    address: string
+    latitude: number
+    longitude: number
+}
+
+interface CreateRentalPointData {
+    name: string
+    location: CreateRentalPointLocation
+}
+
 export const createRentalPoint = (
     requestAddress: Address,
-    coordinates: Coordinate
+    coordinates: { lat: number; lng: number }
 ) => {
-    const requestData = {
+    const requestData: CreateRentalPointData = {
         name: requestAddress.name,
         location: {
             country: requestAddress.country,
             city: requestAddress.city,
             address: requestAddress.address,
-            latitude: Number(coordinates.lat),
-            longitude: Number(coordinates.lng),
+            latitude: coordinates.lat,
+            longitude: coordinates.lng,
         },
     }
+    console.log(requestData)
     return axios
-        .post(`${CREATE_RENTAL_POINT_POST_REQUEST_URL}`, { requestData })
+        .post(`${CREATE_RENTAL_POINT_POST_REQUEST_URL}`, { ...requestData })
         .then((response) => {
             console.log('rp response', response)
         })
+}
+
+export const deleteRentalPoint = (id: string) => {
+    return axios.delete(`${DELETE_RENTAL_POINT_REQUEST_URL}/${id}`, {})
 }

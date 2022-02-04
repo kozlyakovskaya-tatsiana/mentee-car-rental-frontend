290f9931c1d0b57fedc1015193dc2d053fe06466
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-02-04 16:01:51 +0300

    Added uploader photo component

---
+++ car-rental/src/pages/ManagementPage/CarPage/CreateCarForm/RentalPointSelector/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+import { Autocomplete, Box, CircularProgress, TextField } from '@mui/material'
+
+import { getRentalPoints } from 'services/rentalPoint.service'
+import { RentalPointType } from 'shared/types/RentalPoint'
+
+interface FieldProps {
+    value: string
+    readonly?: boolean
+    fieldLabel: string
+    onChange: any
+}
+
+const topFilms: any[] = []
+export const RentalPointSelector: React.FC<FieldProps> = ({
+    value,
+    readonly,
+    fieldLabel,
+    onChange,
+}) => {
+    const [open, setOpen] = React.useState(false)
+    const [options, setOptions] = React.useState<RentalPointType[]>([])
+    const loading = open && options.length === 0
+
+    React.useEffect(() => {
+        let active = true
+
+        if (!loading) {
+            return undefined
+        }
+        ;(async () => {
+            if (active) {
+                getRentalPoints().then((response) => {
+                    console.log(response.data)
+                    setOptions([...response.data])
+                })
+            }
+        })()
+
+        return () => {
+            active = false
+        }
+    }, [loading])
+
+    React.useEffect(() => {
+        if (!open) {
+            setOptions([])
+        }
+    }, [open])
+    return (
+        <Autocomplete
+            id="asynchronous-demo"
+            open={open}
+            onOpen={() => {
+                setOpen(true)
+            }}
+            onClose={() => {
+                setOpen(false)
+            }}
+            isOptionEqualToValue={(option, optValue) =>
+                option.name === optValue.name
+            }
+            getOptionLabel={(option) => option.name}
+            options={options}
+            loading={loading}
+            fullWidth
+            /* eslint-disable react/jsx-fragments */
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label={fieldLabel}
+                    color="secondary"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? (
+                                    <CircularProgress
+                                        color="secondary"
+                                        size={20}
+                                    />
+                                ) : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    )
+}
+RentalPointSelector.defaultProps = {
+    readonly: false,
+}
+
+export default RentalPointSelector
--- car-rental/src/pages/ManagementPage/CarPage/CreateCarForm/index.tsx
+++ car-rental/src/pages/ManagementPage/CarPage/CreateCarForm/index.tsx
@@ -1,39 +1,73 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import * as React from 'react'
+import React from 'react'
+import { ImageListType } from 'react-images-uploading/dist/typings'
 
 import Grid from '@mui/material/Grid'
 
 import { BrandInputType, ModelInputType } from 'shared/types/CarTypes'
 import Fuel from 'shared/enums/Fuel'
-
-import {
-    Button,
-    createFilterOptions,
-    FormControl,
-    Input,
-    TextField,
-    useTheme,
-} from '@mui/material'
-import Box from '@mui/material/Box'
-
 import Transmission from 'shared/enums/Transmission'
+import { getAllCarBrands } from 'services/car.service'
+
+import { Button, createFilterOptions, TextField } from '@mui/material'
+import Zoom from '@material-ui/core/Zoom'
 
 import CarAutocompleteComponent from './CarAurocompleteComponent'
 import CarSelectComponent from './CarSelectComponent'
+import UploaderComponent from './UploaderComponent'
+import RentalPointSelector from './RentalPointSelector'
 
 const filter = createFilterOptions<BrandInputType>()
 
 export const CreateCarForm: React.FC = () => {
-    const theme = useTheme()
+    const [zoomChecked, setZoomChecked] = React.useState<boolean | undefined>()
 
-    const [selectedImage, setSelectedImage] = React.useState<
-        Blob | MediaSource | null
-    >(null)
+    const [brandSelect, setBrandSelect] = React.useState<BrandInputType[]>([])
+    const [modelSelect, setModelSelect] = React.useState<ModelInputType[]>([])
+
+    const maxUploadedPhotos = 6
 
     const [brand, setBrand] = React.useState<BrandInputType | null>(null)
     const [model, setModel] = React.useState<ModelInputType | null>(null)
     const [fuel, setFuel] = React.useState<string>('')
     const [transmission, setTransmission] = React.useState<string>('')
+    const [quantityOfSeats, setQuantityOfSeats] = React.useState<string>('')
+    const [price, setPrice] = React.useState<string>('')
+    const [images, setImages] = React.useState<ImageListType>([])
+    const [rentalPoint, setRentalPoint] = React.useState<string>('')
+
+    React.useEffect(() => {
+        if (brand && model && fuel && transmission && images.length > 0) {
+            setZoomChecked(true)
+        } else {
+            setZoomChecked(false)
+        }
+    }, [brand, model, fuel, transmission, images])
+
+    const onImagesChange = (imageList: ImageListType) => {
+        setImages(imageList)
+    }
+
+    const onPriceChange = (e: any) => {
+        if (Number(e.target.value) < 0) {
+            e.target.value = 0
+        } else {
+            setPrice(e.target.value)
+        }
+    }
+    const onQuantityChange = (e: any) => {
+        if (Number(e.target.value) < 0) {
+            e.target.value = 0
+        } else {
+            setQuantityOfSeats(e.target.value)
+        }
+    }
+
+    React.useEffect(() => {
+        getAllCarBrands().then((response) => {
+            setBrandSelect([...response.data])
+        })
+    }, [])
 
     const onChangeBrand = (event: any, newValue: any) => {
         if (typeof newValue === 'string') {
@@ -111,7 +145,7 @@ export const CreateCarForm: React.FC = () => {
                     filterOptions={filterOptions}
                     optionLabel={optionLabel}
                     renderOptions={renderOptions}
-                    infoArray={[]}
+                    infoArray={brandSelect}
                     label="Car Brand"
                 />
             </Grid>
@@ -122,7 +156,7 @@ export const CreateCarForm: React.FC = () => {
                     filterOptions={filterOptions}
                     optionLabel={optionLabel}
                     renderOptions={renderOptions}
-                    infoArray={[]}
+                    infoArray={modelSelect}
                     label="Model"
                 />
             </Grid>
@@ -146,37 +180,57 @@ export const CreateCarForm: React.FC = () => {
                 <TextField
                     label="Quantity of seats"
                     color="secondary"
+                    type="number"
                     fullWidth
+                    onChange={onQuantityChange}
+                    value={quantityOfSeats}
                 />
             </Grid>
             <Grid item xs={6}>
-                <TextField label="Price per hour" color="secondary" fullWidth />
-            </Grid>
-            <Grid item xs={12}>
-                {selectedImage && (
-                    <div>
-                        <img
-                            alt="not fount"
-                            width="250px"
-                            src={URL.createObjectURL(selectedImage)}
-                        />
-                        <br />
-                        <button
-                            onClick={() => setSelectedImage(null)}
-                            type="button"
-                        >
-                            Remove
-                        </button>
-                    </div>
-                )}
-                <Input
-                    type="file"
-                    name="photo-uploader"
-                    onChange={(event) => {
-                        console.log(event.target.value)
-                    }}
+                <TextField
+                    label="Price per hour"
+                    color="secondary"
+                    type="number"
+                    fullWidth
+                    onChange={onPriceChange}
+                    value={price}
                 />
             </Grid>
+            <Grid item xs={12}>
+                <UploaderComponent
+                    images={images}
+                    onImagesChange={onImagesChange}
+                    maxUploadedPhotos={maxUploadedPhotos}
+                />
+            </Grid>
+            <Zoom
+                in={zoomChecked}
+                style={{ transitionDelay: zoomChecked ? '300ms' : '0ms' }}
+            >
+                <Grid container item component="div" rowSpacing={1}>
+                    <Grid item xs={12}>
+                        <RentalPointSelector
+                            value={rentalPoint}
+                            readonly
+                            fieldLabel="Rental point"
+                            onChange={(e: any) =>
+                                setRentalPoint(e.target.value)
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            type="submit"
+                            onClick={() => console.log('send to back')}
+                            fullWidth
+                            color="secondary"
+                            variant="contained"
+                        >
+                            Submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Zoom>
         </Grid>
     )
 }
